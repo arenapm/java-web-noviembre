dc38ddae874f4958325866da9e4d7849bafe8968
Martin Arena <arena.martin@hotmail.com>
2020-11-29 10:54:53 -0300

    clase7

--- app-jsf-webapp/src/main/java/ar/com/educacionit/jsf/web/managedbeans/ProductoBean.java
+++ app-jsf-webapp/src/main/java/ar/com/educacionit/jsf/web/managedbeans/ProductoBean.java
@@ -2,11 +2,19 @@ package ar.com.educacionit.jsf.web.managedbeans;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
 
+import org.primefaces.event.RowEditEvent;
+import org.primefaces.event.SelectEvent;
+
 import ar.com.educacionit.domain.Producto;
+import ar.com.educacionit.domain.TipoProducto;
 import ar.com.educacionit.services.ProductoService;
 import ar.com.educacionit.services.exceptions.ServiceException;
 import ar.com.educacionit.services.impl.ProductoServiceImpl;
@@ -20,6 +28,15 @@ public class ProductoBean {
 	private String mensajeError;
 	
 	private Producto producto = new Producto();
+	 
+	private Long tipoProducto;
+	
+	private List<Producto> productos;
+	
+	@PostConstruct
+	public void loadProductos() {
+		this.productos = findProductos();
+	}
 	
 	public List<Producto> findProductos() {
 		try {
@@ -33,7 +50,11 @@ public class ProductoBean {
 	public String updateProducto() {
 		
 		String target = "listado-productos?feces-redirect=true";
-		try {
+		try {			
+			TipoProducto nuevoTipoProducto = new TipoProducto();
+			nuevoTipoProducto.setId(this.tipoProducto);
+			
+			this.producto.setTipoProducto(nuevoTipoProducto);
 			this.ps.updateProducto(this.producto);
 		} catch (ServiceException e) {
 			this.mensajeError = e.getCause().getMessage();
@@ -60,6 +81,10 @@ public class ProductoBean {
 	public String crearNuevoProducto() {
 		String target = "listado-productos";
 		try {
+			TipoProducto nuevoTipoProducto = new TipoProducto();
+			nuevoTipoProducto.setId(this.tipoProducto);
+			
+			this.producto.setTipoProducto(nuevoTipoProducto);
 			this.ps.crearProducto(this.producto);
 		} catch (ServiceException e) {
 			this.mensajeError = e.getCause().getMessage();
@@ -88,5 +113,73 @@ public class ProductoBean {
 		this.producto = producto;
 	}
 	
+	public TipoProducto[] getTipoProductos() {
+		TipoProducto[] tiposProductos;
+		try {
+			tiposProductos = this.ps.findTipoProductos()
+					.stream()
+					.collect(Collectors.toSet())
+					.toArray(new TipoProducto[] {});
+		} catch (ServiceException e) {
+			tiposProductos = new TipoProducto[] {};
+		}
+		return tiposProductos;
+	}
+
+	public Long getTipoProducto() {
+		return tipoProducto;
+	}
+
+	public void setTipoProducto(Long tipoProducto) {
+		this.tipoProducto = tipoProducto;
+	}
+
+	public List<Producto> getProductos() {
+		return productos;
+	}
+
+	public void setProductos(List<Producto> productos) {
+		this.productos = productos;
+	}
 	
+	//METODOS AGREGADOS PARA AJAX: PRIMEFACES
+	public void onRowEdit(RowEditEvent<Producto> event) {
+		FacesMessage msg;
+		Producto productoSeleccionado = event.getObject();
+		try {
+			if(!productoSeleccionado.getTipoProducto().getId().equals(this.tipoProducto)) {
+				productoSeleccionado.getTipoProducto().setId(this.tipoProducto);
+			}
+			this.ps.updateProducto(productoSeleccionado);
+			msg = new FacesMessage("Producto editado ", productoSeleccionado.getId().toString());
+			this.productos = this.findProductos();
+		} catch (ServiceException e) {
+			msg = new FacesMessage(e.getMessage(), productoSeleccionado.getId().toString());
+		}
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+
+	public void onRowCancel(RowEditEvent<Producto> event) {
+		Producto productoSeleccionado = event.getObject();
+        FacesMessage msg = new FacesMessage("Edit Cancelled", productoSeleccionado.getId().toString());
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+	
+	public void onRowSelect(SelectEvent<Producto> event) {
+		this.producto = event.getObject();
+    }
+	
+	public void eliminarProducto() {
+		
+		FacesMessage msg;
+		try {
+			this.ps.eliminarProducto(producto.getCodigo());
+			msg = new FacesMessage("Producto eliminado ", producto.getId().toString());
+			this.productos.remove(producto);
+			this.producto = null;
+		} catch (Exception e) {
+			msg = new FacesMessage("Error eliminando producto", e.getMessage());
+		}
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+	}
 }
